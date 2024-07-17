a7a8b78e082d10c6641b5fa05fec7bc1ab0a4c33
marcin-haa <halonmarcin@outlook.com>
2024-07-17 10:03:42 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+// Plik login.php
+
+// Wczytanie użytkowników i haseł z pliku tekstowego
+$users = file('users.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$credentials = [];
+foreach ($users as $user) {
+    list($username, $password) = explode(',', $user);
+    $credentials[$username] = $password;
+}
+
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+
+if (isset($credentials[$username]) && $credentials[$username] === $password) {
+    echo 'Zalogowano pomyślnie!';
+} else {
+    echo 'Nieprawidłowa nazwa użytkownika lub hasło.';
+}
+?>
